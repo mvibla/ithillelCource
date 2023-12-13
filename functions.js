74988f6f35e8a43447612a5c17bf12ccf9c74402
Marta Vibla <mvibla@allvuesystems.com>
2023-12-13 19:01:50 +0200

    homework3 FizzBuzzfunctiom

---
+++ functions.js
@@ -0,0 +1,87 @@
+function FizzBuzz(){
+for (i = 0; i <=100; i++){
+    if(i%15 ===0)
+    console.log("FizzBuzz"); 
+    else if(i%3 === 0)
+    console.log("Fizz");
+    else if(i%5 === 0)
+    console.log("Buzz"); 
+    else 
+    console.log(i);  
+  }
+}
+
+FizzBuzz()
+
+// // //JSFunctions
+// getAge = function () {
+// //     console.log("my age is 28");
+//  return "my age is 36";
+// }
+// // age = getAge();
+
+// function echo () {
+//     return getAge;
+// }
+// ec = echo();
+// console.log(ec());
+// getAge = (age, Name) => {
+//     return `my age is ${age}`;
+// }
+
+// console.log(getAge(35, "Marta"))
+// console.log(getAge("Marta"))
+
+// function call() {
+//     console.log("this is call function");
+
+//     function nestedCall(){
+//         console.log("this is nested function")
+//     }
+//     return nestedCall
+// }
+// call()()
+
+// function call() {
+//     call();
+// }
+
+// 5! = 5*4*3*2*1, 3! = 3*2*1
+
+// function factorial(num){
+// if(num = 1){
+//     return 1;
+// }
+
+// factorial(num - 1);
+// }
+
+// factorial(3)
+
+//message = age > 18 ? "you can drink beer" : "NONONO! You are too young!"
+
+// a = 0;
+
+
+// a += 5;
+
+// console.log(a);
+
+// a = 100;
+
+// console.log(a);
+
+// 0 - 100, 3 - Fizz, 5 - Buzz, 3,5 - FizzBazz
+
+// for (i = 0; i <=100; i++){
+//   if(i%15 ===0)
+//   console.log("FizzBuzz"); 
+//   else if(i%3 === 0)
+//   console.log("Fizz");
+//   else if(i%5 === 0)
+//   console.log("Buzz"); 
+//   else 
+//   console.log(i);
+
+  
+// }
